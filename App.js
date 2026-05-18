bd3d97e577cb8c730e081b13e1fba981224935b2
Xandre2508 <almeidaalexandre1233@gmail.com>
2026-05-18 15:19:26 +0100

    App Update para novas Interfaces

--- App.js
+++ App.js
@@ -2,9 +2,13 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 // Importação dos nossos ecrãs
+import AdminDashboard from './src/screens/AdminDashboard';
 import AuthScreen from './src/screens/AuthScreen';
+import OperatorDashboard from './src/screens/OperatorDashboard';
+import ProfileScreen from './src/screens/ProfileScreen';
 import RescuerDashboard from './src/screens/RescuerDashboard';
 import VictimDashboard from './src/screens/VictimDashboard';
+
 const Stack = createStackNavigator();
 
 export default function App() {
@@ -16,7 +20,10 @@ export default function App() {
         <Stack.Screen name="Login" component={AuthScreen} />
         <Stack.Screen name="VictimDashboard" component={VictimDashboard} />
         <Stack.Screen name="RescuerDashboard" component={RescuerDashboard} />
-        
+        <Stack.Screen name="AdminDashboard" component={AdminDashboard} />
+        <Stack.Screen name="OperatorDashboard" component={OperatorDashboard} />
+        <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
